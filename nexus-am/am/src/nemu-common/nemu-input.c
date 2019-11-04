5cb73c3e1332bc0630c131919c2c23156a5e04b3
Li Linchen <llc_legend@163.com>
2019-11-04 23:26:15 +0800

    [amtest] pass keyboard test
    Signed-off-by: Li Linchen <llc_legend@163.com>

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define KEYDOWN_MASK 0x8000
 
@@ -8,8 +9,13 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      uint32_t val = inl(KBD_ADDR);
+      kbd->keydown = (val & KEYDOWN_MASK);
+      if (kbd->keydown) {
+        kbd->keycode = (val ^ KEYDOWN_MASK);
+      } else {
+        kbd->keycode = val;
+      }
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
