75061eef1630f8578cd73d4e60f63fdef893849f
Li Linchen <llc_legend@163.com>
2019-11-25 18:33:14 +0800

    [nanos-lite] implement simple fixed-size file system
    Signed-off-by: Li Linchen <llc_legend@163.com>

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,14 +1,18 @@
 #include "fs.h"
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
 typedef struct {
-  char *name;
-  size_t size;
-  size_t disk_offset;
+  char *name;          // 文件名
+  size_t size;         // 文件大小
+  size_t disk_offset;  // 文件在ramdisk中的偏移
   ReadFn read;
   WriteFn write;
+  size_t open_offset;  // 文件被打开之后的读写指针
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -35,4 +39,79 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+int fs_open(const char *pathname, int flags, int mode)
+{
+  int fd = -1;
+  assert(pathname);
+  for (int i = 0; i < NR_FILES; i++) {
+    if (strcmp(pathname, file_table[i].name) == 0) {
+      fd = i;
+      break;
+    }
+  }
+
+  return fd;
+}
+
+ssize_t fs_read(int fd, void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+  assert(buf);
+
+  size_t remain = file_table[fd].size - file_table[fd].open_offset;
+  len = (remain < len ? remain : len);
+
+  //printk("[read] read %x bytes from offset %x to %p\n", 
+  //  len, file_table[fd].open_offset, buf);
+  ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+
+  return len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  assert(fd >= 0 && fd < NR_FILES);
+  size_t ret = (size_t)-1;
+
+  // due to out fs cannot change size, seek beyond file is not allowed
+  if (whence == SEEK_SET) {
+    assert(offset <= file_table[fd].size);
+    //printk("[lseek(set)] set offset to %x\n", offset);
+    file_table[fd].open_offset = offset;
+    ret = offset;
+  } else if (whence == SEEK_CUR) {
+    assert(file_table[fd].open_offset + offset <= file_table[fd].size);
+    //printk("[lseek(cur)] set offset to %x\n", file_table[fd].open_offset + offset);
+    file_table[fd].open_offset += offset;
+    ret = file_table[fd].open_offset;
+  } else if (whence == SEEK_END) {
+    assert(offset == 0);
+    //printk("[lseek(end)] set offset to %x\n", file_table[fd].size + offset);
+    file_table[fd].open_offset = file_table[fd].size + offset;
+    ret = file_table[fd].open_offset;
+  } else {
+    assert(0);
+  }
+
+  return ret;
+}
+
+ssize_t fs_write(int fd, const void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+  assert(buf);
+  assert(file_table[fd].open_offset + len <= file_table[fd].size);
+
+  //printk("[write] Write %x bytes to offset %x\n", len, file_table[fd].open_offset);
+  ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+
+  return len;
+}
+
+int fs_close(int fd) {
+  assert(fd >= 0 && fd < NR_FILES);
+  file_table[fd].open_offset = 0;
+
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include "proc.h"
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __ISA_AM_NATIVE__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,33 +10,38 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-size_t get_ramdisk_size();
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  int fd = fs_open(filename, 0, 0);
+  assert(fd >= 0);
+
   Elf_Ehdr ehdr = {0};
-  ramdisk_read(&ehdr, 0, sizeof(ehdr));
-  
+  fs_read(fd, &ehdr, sizeof(ehdr));
   uintptr_t entry_point = (uintptr_t)ehdr.e_entry;
   printk("ELF entry point: %x\n", ehdr.e_entry);
   printk("ELF program header offset: %d, entsize: %d, number: %d\n", 
     ehdr.e_phoff, ehdr.e_phentsize, ehdr.e_phnum);
   
+  fs_lseek(fd, ehdr.e_phoff, SEEK_SET);
   for (int i = 0; i < ehdr.e_phnum; i++) {
     Elf_Phdr phdr = {0};
     assert(sizeof(phdr) == ehdr.e_phentsize);
-    ramdisk_read(&phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(phdr));
-
+    
+    fs_read(fd, &phdr, sizeof(phdr));
     if (phdr.p_type != PT_LOAD) {
       continue;
     }
-    printk("Program header [%d]: type %x, file offset %d, filesize %d, mem %x, memsize %d\n",
+    size_t read_offset = fs_lseek(fd, 0, SEEK_CUR);
+    printk("Program header [%d]: type %x, file offset %x, filesize %x, mem %x, memsize %x\n",
       i, phdr.p_type, phdr.p_offset, phdr.p_filesz, phdr.p_vaddr, phdr.p_memsz);
     
-    ramdisk_read((void*)(phdr.p_vaddr), phdr.p_offset, phdr.p_filesz);
+    fs_lseek(fd, phdr.p_offset, SEEK_SET);
+    fs_read(fd, (void*)(phdr.p_vaddr), phdr.p_filesz);
     memset((void*)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+
+    fs_lseek(fd, read_offset, SEEK_SET);
   }
 
+  fs_close(fd);
   return entry_point;
 }
 
